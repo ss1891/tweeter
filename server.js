1c44f17b8e64c084ea5aac14ab3a12a1c115e56e
ss1891 <simon.salloum@hotmail.se>
2014-05-23 16:21:58 +0100

    added a user model

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
+// Setup
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017');
+
+var User = require('./app/models/user');
+
 app.use(bodyParser()); 
 
 var port = process.env.PORT || 3000;
+
+// Routes
 var router = express.Router();
+
+router.use(function(req, res, next) {
+	console.log('Something is happening.');
+	next(); // Go to next route, don't stop here
+});
 
 router.get('/', function(req, res) {
 	res.json({ message: 'hooray! our API works!' });
 });
-
-// Add more routes here
 
 // Register routes
 app.use('/api', router);
